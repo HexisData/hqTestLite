0e39c442b76f7c3f83564022e3d0b8c75874999a
jscroft <jscroft@hotquant.com>
2016-11-16 11:25:59 -0600

    Add files via upload

---
+++ SampleTest.ps1
@@ -0,0 +1,16 @@
+﻿# Locate the hqTestLite module in a central repository to make it easy to upgrade.
+Import-Module "\\netshare\hqTestLite.psm1" -Force
+
+# Override global defaults with values specific to the test environment.
+Invoke-Expression ".\EnvironmentOverrides.ps1"
+
+# Using global defaults for ProcessAGentPath, DbServer, and DbName.
+# Leaving out directory params assuming SQL scripts are co-located with the test script. 
+Test-MedmSolution `
+    -SetupSqlFiles "MySolution_Setup.sql" `
+    -SolutionName "MySolution" `
+    -SolutionParams "Param1=Value:Param2=Value2:Param3=Value3" `
+    -ResultSqlFiles "MySolution_Result1.sql,MySolution_Result2.sql" `
+    -CleanupSqlFiles "MySolution_Cleanup.sql" `
+    -TestResultPath "MySolution_TestResult.txt" `
+    -CertifiedResultPath "MySolution_CertifiedResult.txt" `

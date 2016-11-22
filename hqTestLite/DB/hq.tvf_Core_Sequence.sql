5942473d2808046f3dbb2c5f74e74145ca4f556b
jscroft <jscroft@HQ-WS-JASON-00>
2016-11-22 18:50:09 +0000

    Added helper DB objects

---
+++ hqTestLite/DB/hq.tvf_Core_Sequence.sql
@@ -0,0 +1,32 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE FUNCTION [hq].[tvf_Core_Sequence] 
+(	
+	@n int
+)
+RETURNS TABLE 
+RETURN 
+(
+	WITH A(n) AS
+	(
+		SELECT 1 
+
+		UNION ALL
+
+		SELECT A.n * 2
+		FROM 
+			A
+			CROSS APPLY (
+				SELECT n = 1 
+				UNION ALL SELECT 1 
+			) B
+		WHERE A.n < @n
+	)
+
+	SELECT TOP(@n)
+		i = ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) 
+	FROM A
+)
+GO

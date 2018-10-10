9cdb69fe2c8947875a1013ddc6a31796481f7fa3
Jason Williscroft <JasonW@paraport.com>
2018-10-09 20:06:12 -0400

    Added support for negative numbers

--- GenerateTestData.ps1
+++ GenerateTestData.ps1
@@ -9,14 +9,14 @@ $DbName = Read-UserEntry -Label "Database Name" -Default $Global:DefaultMedmDbNa
 $TableSchema = Read-UserEntry -Label "Table Schema" -Default "dbo"
 $TableName = Read-UserEntry -Label "Table Name"
 $ColNamePattern = Read-UserEntry -Label "Column Name Exclusion Pattern" -Default "^CADIS_SYSTEM_|.*RUN_?ID" -Pattern ".*"
-$CsvPath = Read-UserEntry -Label "CSV Output Path" -Default "Test.csv"
-$RowCount = Read-UserEntry -Label "Row Count" -Default "10" -Pattern "\d+"
+$CsvPath = Read-UserEntry -Label "CSV Output Path" -Pattern "\S+\.csv"
+$RowCount = Read-UserEntry -Label "Row Count" -Default "10" -Pattern "-?\d+"
 $MinDate = Read-UserEntry -Label "Min Date" -Default "2018-01-01T00:00:00" -Pattern "\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}(:\d{2})?)?"
 $MaxDate = Read-UserEntry -Label "Min Date" -Default "2027-12-31T23:59:59" -Pattern "\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}(:\d{2})?)?"
-$MinDec = Read-UserEntry -Label "Min Decimal" -Default "0.0" -Pattern "\d+(\.\d*)?"
-$MaxDec = Read-UserEntry -Label "Max Decimal" -Default "10.0" -Pattern "\d+(\.\d*)?"
-$MinInt = Read-UserEntry -Label "Min Integer" -Default "1000" -Pattern "\d+"
-$MaxInt = Read-UserEntry -Label "Max Integer" -Default "9999" -Pattern "\d+"
+$MinDec = Read-UserEntry -Label "Min Decimal" -Default "0.0" -Pattern "-?\d+(\.\d*)?"
+$MaxDec = Read-UserEntry -Label "Max Decimal" -Default "10.0" -Pattern "-?\d+(\.\d*)?"
+$MinInt = Read-UserEntry -Label "Min Integer" -Default "1000" -Pattern "-?\d+"
+$MaxInt = Read-UserEntry -Label "Max Integer" -Default "9999" -Pattern "-?\d+"
 $MaxStrLen = Read-UserEntry -Label "Max String Length" -Default "32" -Pattern "\d+"
 
 Export-CsvTestData `
@@ -28,8 +28,8 @@ Export-CsvTestData `
     -ColNamePattern $ColNamePattern `
     -CsvPath $CsvPath `
     -RowCount ([int]$RowCount) `
-    -MinDate ([datetime]::ParseExact($MinDate, "yyyy-MM-dd HH:mm:ss", $null)) `
-    -MaxDate ([datetime]::ParseExact($MaxDate, "yyyy-MM-dd HH:mm:ss", $null)) `
+    -MinDate ([datetime]::ParseExact($MinDate, "yyyy-MM-ddTHH:mm:ss", $null)) `
+    -MaxDate ([datetime]::ParseExact($MaxDate, "yyyy-MM-ddTHH:mm:ss", $null)) `
     -MinDec ([decimal]$MinDec) `
     -MaxDec ([decimal]$MaxDec) `
     -MinInt ([int]$MinInt) `

50139089589a14bb16b684cb1cb58825ba66be98
Jason Williscroft <JasonW@paraport.com>
2018-10-12 11:41:25 -0400

    Added support for environmental file drop paths

--- Local/hqTestLite/config.ps1
+++ Local/hqTestLite/config.ps1
@@ -1,2 +1,4 @@
 ﻿$Global:DefaultMedmDbServer = "MyServerName"
 $Global:DefaultMedmDbName = "MyDbName"
+$Global:BloombergPxFileDropDir = "\\netshare\bloomberg\price\IN"
+$Global:BloombergSecFileDropDir = "\\netshare\bloomberg\security\IN"
